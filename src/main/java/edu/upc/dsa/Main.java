91cb48fd9232f6d4f70ed14bc4eb8c94cd91ca6c
krunal badsiwal <krunalmiracle@gmail.com>
2020-03-25 20:20:57 +0100

    Interceptor Implementation Everything working as expected, learned how to add OkHttp client and a interceptor to log, profile and audit .

--- src/main/java/edu/upc/dsa/Main.java
+++ src/main/java/edu/upc/dsa/Main.java
@@ -1,27 +1,33 @@
 package edu.upc.dsa;
 
-
+import okhttp3.OkHttpClient;
+import okhttp3.logging.HttpLoggingInterceptor;
 import retrofit2.Call;
 import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
-import java.awt.peer.SystemTrayPeer;
-import java.io.IOException;
-import java.rmi.server.ExportException;
 import java.util.List;
 
 public class Main {
 
     public static void main(String[] args) {
-        // write your code here
+        //Adding Interceptor
+        HttpLoggingInterceptor interceptor = new HttpLoggingInterceptor();
+        interceptor.setLevel(HttpLoggingInterceptor.Level.BODY);
+        //Attaching Interceptor to a client
+        OkHttpClient client = new OkHttpClient().newBuilder().addInterceptor(interceptor).build();
+
+        // Running Retrofit to get result from Github service Interface
         Retrofit retrofit = new Retrofit.Builder()
                 .baseUrl("https://api.github.com/")
                 .addConverterFactory(GsonConverterFactory.create())
+                .client(client)
                 .build();
 
         GitHubService service = retrofit.create(GitHubService.class);
         Call<List<Repo>> repos = service.listRepos("krunalmiracle");
         try {
             List<Repo> result = repos.execute().body();
+            assert result != null;
             for (Repo r:result) {
                 System.out.print(r.toString());
             }
